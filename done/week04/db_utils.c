e65d6c9b8b2f0b39e62a9bb9b79c6edab2992420
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-03-13 13:54:45 +0100

    deleted useless whitespaces

--- done/week04/db_utils.c
+++ done/week04/db_utils.c
@@ -35,11 +35,11 @@ sha_to_string (const unsigned char* SHA,
 void print_header(const struct pictdb_header header) {
 	printf("*****************************************\n"
 			"**********DATABASE HEADER START**********\n"
-			"DB NAME: %31s \n"
-			"VERSION: %" PRIu32 " \n"
-			"IMAGE COUNT: %" PRIu32 " \t\t MAX IMAGES: %" PRIu32 "\n"
-			"THUMBNAIL: %" PRIu16 " x %" PRIu16" \t SMALL: %" PRIu16 " x %"PRIu16" \n"
-			"***********DATABASE HEADER END*********** \n"
+			"DB NAME: %31s\n"
+			"VERSION: %"PRIu32"\n"
+			"IMAGE COUNT: %"PRIu32"\t\tMAX IMAGES: %"PRIu32"\n"
+			"THUMBNAIL: %"PRIu16" x %"PRIu16"\tSMALL: %"PRIu16" x %"PRIu16"\n"
+			"***********DATABASE HEADER END***********\n"
 			"*****************************************\n",
 			header.db_name, header.db_version, header.num_files,
 			header.max_files, header.res_resized[0], header.res_resized[1],
@@ -54,14 +54,14 @@ void print_metadata (const struct pict_metadata metadata) {
     sha_to_string(metadata.SHA, sha_printable);
 	
 	printf(
-			"PICTURE ID: %s \n"
-			"SHA: %s \n"
-			"VALID: %" PRIu16" \n"
-			"UNUSED: %" PRIu16" \n"
-			"OFFSET ORIG. : %" PRIu64" \t\t SIZE ORIG. : %" PRIu32"\n"
-			"OFFSET THUMB.: %" PRIu64" \t\t SIZE THUMB.: %" PRIu32"\n"
-			"OFFSET SMALL : %" PRIu64" \t\t SIZE SMALL : %" PRIu32"\n"
-			"ORIGINAL: %"PRIu32" x %"PRIu32" \n"
+			"PICTURE ID: %s\n"
+			"SHA: %s\n"
+			"VALID: %"PRIu16"\n"
+			"UNUSED: %"PRIu16"\n"
+			"OFFSET ORIG. : %"PRIu64" \t\t SIZE ORIG. : %"PRIu32"\n"
+			"OFFSET THUMB.: %"PRIu64" \t\t SIZE THUMB.: %"PRIu32"\n"
+			"OFFSET SMALL : %"PRIu64" \t\t SIZE SMALL : %"PRIu32"\n"
+			"ORIGINAL: %"PRIu32" x %"PRIu32"\n"
 			"*****************************************\n",
 			metadata.pict_id, sha_printable, metadata.is_valid,
 			metadata.unused_16, 
