807d2fbc9d52f40df24443c635e43bfe4684dd31
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-03-13 13:47:27 +0100

    Think I solved the weird indentation issue

--- done/week04/db_list.c
+++ done/week04/db_list.c
@@ -13,7 +13,7 @@
 void do_list(const struct pictdb_file db_file) {
 	print_header(db_file.header);
 	if (db_file.header.num_files == 0) {
-		printf("<< empty database >>");
+		printf("<< empty database >>\n");
 	} else {
 		for (size_t i = 0; i < MAX_MAX_FILES; ++i) {
 			if (db_file.metadata[i].is_valid == NON_EMPTY)
