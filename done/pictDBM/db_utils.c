07efb07a5a0a09ec52c0531c405c7e53aa65db10
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-04-03 17:55:47 +0200

    pre handin

--- done/pictDBM/db_utils.c
+++ done/pictDBM/db_utils.c
@@ -53,7 +53,7 @@ void print_header(const struct pictdb_header header) {
 void print_metadata (const struct pict_metadata metadata) {
     char sha_printable[2 * SHA256_DIGEST_LENGTH + 1];
     sha_to_string(metadata.SHA, sha_printable);
-	
+
 	printf(
 			"PICTURE ID: %s\n"
 			"SHA: %s\n"
@@ -65,9 +65,9 @@ void print_metadata (const struct pict_metadata metadata) {
 			"ORIGINAL: %" PRIu32 " x %" PRIu32 "\n"
 			"*****************************************\n",
 			metadata.pict_id, sha_printable, metadata.is_valid,
-			metadata.unused_16, 
+			metadata.unused_16,
 			metadata.offset[RES_ORIG], metadata.size[RES_ORIG],
 			metadata.offset[RES_THUMB], metadata.size[RES_THUMB],
 			metadata.offset[RES_SMALL], metadata.size[RES_SMALL],
-			metadata.res_orig[0], metadata.res_orig[1]);			
+			metadata.res_orig[0], metadata.res_orig[1]);
 }
