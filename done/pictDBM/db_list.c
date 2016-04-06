1a2bf293bd501af08983d3eb7eb7ff0c2b2233a4
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-04-06 21:05:48 +0200

    Format with A8 style

--- done/pictDBM/db_list.c
+++ done/pictDBM/db_list.c
@@ -10,16 +10,18 @@
 #include <stdio.h>
 #include "pictDB.h"
 
-void do_list(const struct pictdb_file* db_file) {
-        // Print header
-	print_header(&db_file->header);
-	if (db_file->header.num_files == 0) {
-		printf("<< empty database >>\n");
-	} else {
-                // iterate over all elements in array of metadata and print them
-		for (size_t i = 0; i < MAX_MAX_FILES; ++i) {
-			if (db_file->metadata[i].is_valid == NON_EMPTY)
-				print_metadata(&db_file->metadata[i]);
-		}
-	}
+void do_list(const struct pictdb_file* db_file)
+{
+    // Print header
+    print_header(&db_file->header);
+    if (db_file->header.num_files == 0) {
+        printf("<< empty database >>\n");
+    } else {
+        // iterate over all elements in array of metadata and print them
+        // only if they are valid
+        for (size_t i = 0; i < MAX_MAX_FILES; ++i) {
+            if (db_file->metadata[i].is_valid == NON_EMPTY)
+                print_metadata(&db_file->metadata[i]);
+        }
+    }
 }
