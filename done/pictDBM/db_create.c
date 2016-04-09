da72281fdfecfce3b61dc9c399a3f301a9b79c80
panv <panv@hotmail.fr>
2016-04-09 16:40:20 +0200

    Update do_create with master changes

--- done/pictDBM/db_create.c
+++ done/pictDBM/db_create.c
@@ -10,12 +10,18 @@
 
 #include <string.h> // for strncpy
 
+// Prototype
+struct pict_metadata empty_metadata(void);
+
 /********************************************************************//**
  * Creates the database called db_filename. Writes the header and the
  * preallocated empty metadata array to database file.
  */
 
-int do_create(const char filename[], struct pictdb_file* db_file) {
+int do_create(const char* filename, struct pictdb_file* db_file) {
+    if (filename == NULL || strlen(filename) > MAX_DB_NAME) {
+        return ERR_INVALID_FILENAME;
+    }
     // Sets the DB header name
     strncpy(db_file->header.db_name, filename,  MAX_DB_NAME);
     db_file->header.db_name[MAX_DB_NAME] = '\0';
@@ -24,34 +30,35 @@ int do_create(const char filename[], struct pictdb_file* db_file) {
     db_file->header.db_version = 0;
     db_file->header.num_files = db_file->header.max_files;
 
+    for (uint32_t i = 0; i < db_file->header.num_files; ++i) {
+        db_file.metadata[i] = empty_metadata();
+    }
+    
     // Open stream and check for errors
     FILE* output = fopen(filename, "wb");
     if (output == NULL) {
         fprintf(stderr,
                 "Impossible ouvrir le fichier %s en écriture\n", filename);
-        return ERR_INVALID_FILENAME;
+        return ERR_IO;
     }
-
-    // No need to initialize .metadata
-    size_t total = 0; // Used to store the total number of object written by fwrite
 
     // Write header and check for error
-    size_t written_els = fwrite(&db_file->header, sizeof(db_file->header), 1, output);
-    if (written_els != 1) {
-        fprintf(stderr, "Impossible créer la base de données %s\n", db_file->header.db_name);
-        return ERR_IO;
-    }
-    ++total; // Header counts as one element
-
+    size_t header_ctrl = fwrite(&db_file->header, sizeof(db_file->header), 1, output);
     // Write metadata and check for error
-    written_els = fwrite(&db_file->metadata, sizeof(db_file->metadata), 1, output);
-    if (written_els != 1) {
-        fprintf(stderr, "Impossible créer la base de données %s\n", db_file->header.db_name);
+    size_t metadata_ctrl = fwrite(&db_file->metadata,
+        sizeof(struct pict_metadata), db_file->header.num_files, output);
+    if (header_ctrl != 1 || metadata_ctrl != db_file->header.num_files) {
+        fprintf(stderr, "Error : cannot create database %s\n", db_file->header.db_name);
         return ERR_IO;
     }
-    total += db_file->header.num_files; // ??? Correct
 
     fclose(output);
-    printf("%zu item(s) written\n", total);
+    printf("%zu item(s) written\n", header_ctrl + metadata_ctrl);
     return 0;
+}
+
+struct pict_metadata empty_metadata(void)
+{
+    struct pict_metadata metadata = {.is_valid = EMPTY};
+    return metadata;
 }
