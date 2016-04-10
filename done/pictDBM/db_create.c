8ce0e2666bb2b04eda6d9ad68153586bd7d4d671
panv <panv@hotmail.fr>
2016-04-10 03:01:58 +0200

    Fixes and error check

--- done/pictDBM/db_create.c
+++ done/pictDBM/db_create.c
@@ -24,15 +24,18 @@ int do_create(const char* filename, struct pictdb_file db_file)
         fprintf(stderr, "Error : invalid filename\n");
         return ERR_INVALID_FILENAME;
     }
+    if (db_file.max_files > MAX_MAX_FILES) {
+        return ERR_MAX_FILES;
+    }
     // Sets the DB header name
     strncpy(db_file.header.db_name, filename,  MAX_DB_NAME);
     db_file.header.db_name[MAX_DB_NAME] = '\0';
 
     // Initialize header
     db_file.header.db_version = 0;
-    db_file.header.num_files = db_file.header.max_files;
+    db_file.header.num_files = 0;
     
-    for (uint32_t i = 0; i < db_file.header.num_files; ++i) {
+    for (uint32_t i = 0; i < db_file.header.max_files; ++i) {
         db_file.metadata[i] = empty_metadata();
     }
 
@@ -45,8 +48,8 @@ int do_create(const char* filename, struct pictdb_file db_file)
 
     size_t header_ctrl = fwrite(&db_file.header, sizeof(struct pictdb_header), 1, output);
     size_t metadata_ctrl = fwrite(&db_file.metadata,
-            sizeof(struct pict_metadata), db_file.header.num_files, output);
-    if (header_ctrl != 1 || metadata_ctrl != db_file.header.num_files) {
+            sizeof(struct pict_metadata), db_file.header.max_files, output);
+    if (header_ctrl != 1 || metadata_ctrl != db_file.header.max_files) {
         fprintf(stderr, "Error : cannot create database %s\n", db_file.header.db_name);
         return ERR_IO;
     }
--- done/pictDBM/pictDBM.c
+++ done/pictDBM/pictDBM.c
@@ -75,7 +75,6 @@ int help (void)
            "  list <dbfilename>: list pictDB content.\n"
            "  create <dbfilename>: create a new pictDB.\n");
     return 0;
-    // should we return one of the errors defined in error.h? INVALID_COMMAND?
 }
 
 /********************************************************************//**
