eab5b0a85a2a3bd68ec9ccc39d3bff4a0399d483
panv <panv@hotmail.fr>
2016-04-13 17:37:05 +0200

    Finish write to disk part

--- done/pictDBM/image_content.c
+++ done/pictDBM/image_content.c
@@ -2,6 +2,9 @@
 
 // Prototypes
 int valid_resolution(int resolution);
+long write_to_disk(struct pictdb_file* db_file,
+                   void* to_write, long offset, int whence);
+
 
 int lazily_resize(int resolution, struct pictdb_file* db_file, size_t index) {
     // Error checks on arguments
@@ -25,22 +28,40 @@ int lazily_resize(int resolution, struct pictdb_file* db_file, size_t index) {
 
     // assume db_file.fpdb is open?
     // image = return value of function resizes image
-    long offset = write_to_disk(db_file, image);
+    long offset = write_to_disk(db_file, image, 0, SEEK_END);
     if (offset != -1) {
         db_file->metadata[index].size[resolution] = sizeof(image);
         metadata->offset[resolution] = offset;
-
+        offset = write_to_disk(db_file, db_file->metadata,
+                               sizeof(struct pictdb_header), SEEK_SET);
+        return (offset != -1) ? 0 : ERR_IO;
     }
     return ERR_IO;
 }
 
+/**
+ * @brief Checks whether the given resolution is within the valid range.
+ *
+ * @param resolution The resolution to check.
+ * @return 0 if the resolution is valid, 1 otherwise
+ */
 int valid_resolution(int resolution) {
     return (resolution == RES_THUMB || resolution == RES_SMALL ||
             resolution == RES_ORIG) ? 0 : 1;
 }
 
-long write_to_disk(struct pictdb_file* db_file, void* to_write) {
-    int seek_success = fseek(db_file->fpdb, 0, SEEK_END);
+/**
+ * @brief Writes the data pointed to by the given pointer to disk.
+ *
+ * @param db_file The database containing the file to write to.
+ * @param to_write The pointer pointing to the data to write.
+ * @param offset The offset required by fseek.
+ * @param whence The starting position of the write head.
+ * @return -1 in case of error, the size of the file before writing the data otherwise.
+ */
+long write_to_disk(struct pictdb_file* db_file,
+                   void* to_write, long offset, int whence) {
+    int seek_success = fseek(db_file->fpdb, offset, whence);
     if (seek_success == 0) {
         long offset = ftell(db_file->fpdb);
         int write_success = fwrite(to_write, sizeof(*to_write), 1, db_file->fpdb);
