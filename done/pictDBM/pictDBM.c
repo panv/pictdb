55c09d9e3dc196cf0d0400cced9421e9ee7dc302
panv <panv@hotmail.fr>
2016-04-22 18:29:32 +0200

    Finish new implementation of do_create_cmd
    Weird bug to fix: first attempts to create database worked, but now it segfaults with no recompile.

--- done/pictDBM/pictDBM.c
+++ done/pictDBM/pictDBM.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "error.h"
+#include "pictDBM_tools.h"
 
 #define NB_CMD 4 // Number of command line functions the database possesses
 
@@ -22,6 +23,11 @@ typedef struct {
     const char* command_name;
     command cmd;
 } command_mapping;
+
+// Prototypes
+int parse_create_options(char* option);
+int check_argument_number(int remaining, int expected);
+int check_values(uint16_t x_res, uint16_t y_res, uint16_t max_value);
 
 
 /********************************************************************//**
@@ -53,23 +59,91 @@ int do_create_cmd (int args, char* argv[])
         return ERR_NOT_ENOUGH_ARGUMENTS;
     }
 
-    // This will later come from the parsing of command line arguments
-    const uint32_t max_files =  10;
-    const uint16_t thumb_res =  64;
-    const uint16_t small_res = 256;
+    char* filename = argv[1];
+    args -= 2;
+    argv += 2;
+
+    uint32_t max_files = 0;
+    uint16_t x_thumb_res = 0;
+    uint16_t y_thumb_res = 0;
+    uint16_t x_small_res = 0;
+    uint16_t y_small_res = 0;
+
+    for (size_t i = 0; i < args; ++i) {
+        switch(parse_create_options(argv[i])) {
+            case 1:
+                if (check_argument_number(args - (i + 1), 1) != 0) {
+                    return ERR_NOT_ENOUGH_ARGUMENTS;
+                }
+                max_files = atouint32(argv[i + 1]);
+                if (max_files == 0 || max_files > MAX_MAX_FILES) {
+                    return ERR_MAX_FILES;
+                }
+                i += 1;
+                break;
+            case 2:
+                if (check_argument_number(args - (i + 1), 2) != 0) {
+                    return ERR_NOT_ENOUGH_ARGUMENTS;
+                }
+                x_thumb_res = atouint16(argv[i + 1]);
+                y_thumb_res = atouint16(argv[i + 2]);
+                if (check_values(x_thumb_res, y_thumb_res, 128) != 0) {
+                    return ERR_RESOLUTIONS;
+                }
+                i += 2;
+                break;
+            case 3:
+                if (check_argument_number(args - (i + 1), 2) != 0) {
+                    return ERR_NOT_ENOUGH_ARGUMENTS;
+                }
+                x_small_res = atouint16(argv[i + 1]);
+                y_small_res = atouint16(argv[i + 2]);
+                if (check_values(x_small_res, y_small_res, 512) != 0) {
+                    return ERR_RESOLUTIONS;
+                }
+                i += 2;
+                break;
+            case 0:
+                return ERR_INVALID_ARGUMENT;
+        }
+    }
+
+    const uint32_t default_max_files = max_files == 0 ? 10 : max_files;
+    const uint16_t default_x_thumb_res = x_thumb_res == 0 ? 64 : x_thumb_res;
+    const uint16_t default_y_thumb_res = y_thumb_res == 0 ? 64 : y_thumb_res;
+    const uint16_t default_x_small_res = x_small_res == 0 ? 256: x_small_res;
+    const uint16_t default_y_small_res = y_small_res == 0 ? 256: y_small_res;
+
     puts("Create");
     struct pictdb_header db_header = {
-        .max_files = max_files,
-        .res_resized = {thumb_res, thumb_res, small_res, small_res}
+        .max_files = default_max_files,
+        .res_resized = {default_x_thumb_res, default_y_thumb_res,
+                        default_x_small_res, default_y_small_res}
     };
     struct pictdb_file db_file = {.header = db_header};
 
-    int db_created = do_create(argv[1], &db_file);
+    int db_created = do_create(filename, &db_file);
     if (db_created == 0) {
         print_header(&db_file.header);
     }
 
     return db_created;
+}
+
+int parse_create_options(char* option) {
+    return (strcmp(option, "-max_files") == 0) ? 1 :
+           (strcmp(option, "-thumb_res") == 0) ? 2 :
+           (strcmp(option, "-small_res") == 0) ? 3 :
+           0;
+}
+
+int check_argument_number(int remaining, int expected) {
+    return (remaining < expected) ? 1 : 0;
+}
+
+int check_values(uint16_t x_res, uint16_t y_res, uint16_t max_value) {
+    return (x_res == 0 || y_res == 0
+            || x_res > max_value || y_res > max_value) ? 1 : 0;
 }
 
 /********************************************************************//**
