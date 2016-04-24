045298f45a6c9e0999855a800c5db8987434ce17
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-04-24 22:31:09 +0200

    Reorganize loop. Add useless hash creation function

--- done/pictDBM/dedup.c
+++ done/pictDBM/dedup.c
@@ -1,15 +1,48 @@
 #include "dedup.h"
+#include<stdlib.h>
+#include <openssl/sha.h>
 
-int do_name_and_content_dedup(struct pictdb_file* db_file, uint32_t index) {
-    for (size_t i; i < db_file->header.max_files; ++i) {
-        if (i != index && db_file->metadata[i].is_valid == NON_EMPTY) {
-            if (strcmp(db_file->metadata[i].pict_id,
-                       db_file->metadata[index].pict_id) == 0) {
+int do_name_and_content_dedup(struct pictdb_file* db_file, uint32_t index)
+{
+    struct pict_metadata img_index = db_file->metadata[index];
+    for (size_t i = 0; i < db_file->header.max_files; ++i) {
+        if (i != index) {
+            if (db_file->metadata[i].is_valid == NON_EMPTY && !strcmp(db_file->metadata[i].pict_id, img_index.pict_id)) {
                 return ERR_DUPLICATE_ID;
             }
-            if (qqc) {
-                faire_des_trucs();
+            if (!strncmp(db_file->metadata[i].SHA, img_index.SHA, SHA256_DIGEST_LENGTH)) {
+                // NO DEDUP
+            }
+            else {
+                img_index.offsets[RES_ORIG] = db_file->metadata.offsets[RES_ORIG];
+                img_index.offsets[RES_THUMB] = db_file->metadata.offsets[RES_THUMB];
+                img_index.offsets[RES_SMALL] = db_file->metadata.offsets[RES_SMALL];
+                // img_index.offset[RES_ORIG] = 0;
+                // return 0;
             }
         }
     }
+}
+
+unsigned char* hash_of_image(struct pictdb_file* db_file, uint32_t index)
+{
+    // destination preparation
+    unsigned char* hash = calloc(SHA256_DIGEST_LENGTH, sizeof(char));
+
+    // Initialization of sha reaction
+    SHA256_CTX hash_calculator;
+    SHA256_Init(&hash_calculator);
+
+    // input preparation
+    uint32_t img_size = db_file->metadata[index].size[RES_ORIG];
+    fseek(db_file->fpdb, db_file->metadata[index].offset[RES_ORIG], SEEK_SET);
+    char image[img_size];
+    fread(image, img_size, 1, db_file->fpdb);
+
+    // Run over data
+    SHA256_Update(&hash_calculator, image, img_size);
+
+    //Extract hash
+    SHA256_Final(hash, &hash_calculator);
+    return hash;
 }
