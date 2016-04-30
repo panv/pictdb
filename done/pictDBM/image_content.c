54f84f23a5027327253ddf8b2c1166ed1527ba96
panv <panv@hotmail.fr>
2016-04-30 22:50:19 +0200

    Refactor and comment image_content

--- done/pictDBM/image_content.c
+++ done/pictDBM/image_content.c
@@ -63,51 +63,60 @@ int lazily_resize(int resolution, struct pictdb_file* db_file,
         || index >= db_file->header.max_files) {
         return ERR_INVALID_ARGUMENT;
     }
-    if (db_file->metadata[index].is_valid == EMPTY) {
+    if (db_file->header.num_files == 0
+        || db_file->metadata[index].is_valid == EMPTY) {
         fprintf(stderr, "Error : image not contained in the database");
         return ERR_INVALID_ARGUMENT;
     }
 
-    // If the database is empty, the image already exists in the asked resolution
-    // or the asked resolution is the original resolution, do nothing.
-    if (resolution == RES_ORIG || db_file->header.num_files == 0
+    // If the image already exists in the asked resolution or the asked
+    // resolution is the original resolution, do nothing.
+    if (resolution == RES_ORIG
         || db_file->metadata[index].size[resolution] != 0) {
         return 0;
     }
 
-    uint32_t size = db_file->metadata[index].size[RES_ORIG]; //Used often.
-    char image_in_bytes[size]; //Initialize array for image and read it into it.
+    uint32_t size_orig = db_file->metadata[index].size[RES_ORIG]; // Used often
+
+    // Store the original image in an array
+    char image_in_bytes[size_orig];
     if (fseek(db_file->fpdb, db_file->metadata[index].offset[RES_ORIG], SEEK_SET)
-        || fread(image_in_bytes, size, 1, db_file->fpdb) != 1) {
+        || fread(image_in_bytes, size_orig, 1, db_file->fpdb) != 1) {
         return ERR_IO;
     }
-    size_t output_size = 0; // Resize the image using VIPS
-    void* output_buffer = resize(image_in_bytes, size,
+
+    // Resize the image using VIPS
+    size_t output_size = 0;
+    void* output_buffer = resize(image_in_bytes, size_orig,
                                  db_file->header.res_resized[resolution * 2],
                                  db_file->header.res_resized[resolution * 2 + 1],
                                  &output_size);
     if (output_buffer == NULL) {
         g_free(output_buffer);
-        fprintf(stderr, ERROR_MESSAGES[ERR_VIPS]);
         return ERR_VIPS;
     }
 
-    long offset = write_to_disk(db_file, output_buffer, output_size, 1, 0,
-                                SEEK_END);
-    g_free(output_buffer); // Once written, we can free the memory from the image.
-    if (offset != -1) {
+    // Write the image at the end of the file and get the offset
+    long file_position = write_to_disk(db_file, output_buffer, output_size,
+                                       1, 0, SEEK_END);
+    // Once written, we can free the memory from the image
+    g_free(output_buffer);
+    if (file_position != -1) {
+        // Update the metadata and write it to disk
         db_file->metadata[index].size[resolution] = output_size;
-        db_file->metadata[index].offset[resolution] = offset;
-        offset = write_to_disk(db_file, db_file->metadata,
-                               sizeof(struct pict_metadata),
-                               db_file->header.max_files,
-                               sizeof(struct pictdb_header), SEEK_SET);
-        ++db_file->header.db_version;
-        if (offset != -1 || fseek(db_file->fpdb, 0, SEEK_SET) == 0 ||
-                fwrite(db_file->header, sizeof(struct pictdb_header), 1, db_file->fpdb) == 1) {
-            return 0;
-        } else {
-            return ERR_IO;
+        db_file->metadata[index].offset[resolution] = file_position;
+        file_position = write_to_disk(db_file, &db_file->metadata[index],
+                                      sizeof(struct pict_metadata), 1,
+                                      sizeof(struct pictdb_header)
+                                      + sizeof(struct pict_metadata) * index,
+                                      SEEK_SET);
+        if (file_position != -1) {
+            // Update the header and write it to disk
+            ++db_file->header.db_version;
+            file_position = write_to_disk(db_file, &db_file->header,
+                                          sizeof(struct pictdb_header), 1,
+                                          0, SEEK_SET);
+            return file_position == -1 ? ERR_IO : 0;
         }
     }
     return ERR_IO;
@@ -123,9 +132,9 @@ long write_to_disk(struct pictdb_file* db_file, void* to_write,
                    size_t size, size_t nmemb, long offset, int whence)
 {
     if (fseek(db_file->fpdb, offset, whence) == 0) {
-        long file_size = ftell(db_file->fpdb);
+        long file_position = ftell(db_file->fpdb);
         size_t write_success = fwrite(to_write, size, nmemb, db_file->fpdb);
-        return (write_success == nmemb) ? file_size : -1;
+        return (write_success == nmemb) ? file_position : -1;
     }
     return -1;
 }
@@ -136,7 +145,7 @@ void* resize(void* input_buffer, uint32_t input_size, uint16_t max_x,
     VipsObject* process = VIPS_OBJECT(vips_image_new());
     VipsImage** pics = (VipsImage**) vips_object_local_array(process, 2);
     void* output_buffer;
-    if (vips_jpegload_buffer(input_buffer, input_size, &pics[0], NULL)) {
+    if (vips_jpegload_buffer(input_buffer, input_size, &pics[0], NULL) != 0) {
         g_object_unref(process);
         return NULL;
     }
