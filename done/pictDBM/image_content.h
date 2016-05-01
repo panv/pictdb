76242d21504de14467e47337f1fee4dff97f85d5
panv <panv@hotmail.fr>
2016-05-01 16:06:48 +0200

    Comment get_resolution prototype in header

--- done/pictDBM/image_content.h
+++ done/pictDBM/image_content.h
@@ -5,6 +5,7 @@
  * @author Vincenzo Bazzucchi
  * @author Nicolas Phan Van
  */
+
 #include "pictDB.h"
 
 /**
@@ -20,4 +21,14 @@
 int lazily_resize(int resolution, struct pictdb_file* db_file,
                   size_t index);
 
-int get_resolution(uint32_t* height, uint32_t* width, const char* image_buffer, size_t image_size);
+/**
+ * @brief Retrieves the resolution (width and height) of an image.
+ *
+ * @param height       Location where the height of the image will be stored.
+ * @param width        Location where the width of the image will be stored.
+ * @param image_buffer Pointer to the memory location of the image.
+ * @param image_size   The size of the image (in bytes).
+ * @return 0 if the operation was successful, ERR_VIPS otherwise.
+ */
+int get_resolution(uint32_t* height, uint32_t* width, const char* image_buffer,
+                   size_t image_size);
