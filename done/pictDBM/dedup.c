80f31cf977dd04531e0d50fabc86ef83ec611289
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-05-02 17:00:29 +0200

    Fix stupid distraction error

--- done/pictDBM/dedup.c
+++ done/pictDBM/dedup.c
@@ -22,26 +22,26 @@ int do_name_and_content_dedup(struct pictdb_file* db_file, uint32_t index)
         return ERR_INVALID_ARGUMENT;
     }
 
-    struct pict_metadata img_index = db_file->metadata[index];
+    struct pict_metadata* img_index = &db_file->metadata[index];
     for (size_t i = 0; i < db_file->header.max_files; ++i) {
         // For all valid images other than the one at index
         if (i != index && db_file->metadata[i].is_valid == NON_EMPTY) {
             // Two distinct images have the same ID!
             if (strncmp(db_file->metadata[i].pict_id,
-                        img_index.pict_id, MAX_PIC_ID) == 0) {
+                        img_index->pict_id, MAX_PIC_ID) == 0) {
                 return ERR_DUPLICATE_ID;
-            } else if (!hashcmp(db_file->metadata[i].SHA, img_index.SHA)) {
+            } else if (!hashcmp(db_file->metadata[i].SHA, img_index->SHA)) {
                 // Two images with the same hash: deduplication
                 for (size_t res = 0; res < NB_RES; ++res) {
-                    img_index.offset[res] = db_file->metadata[i].offset[res];
-                    img_index.size[res] = db_file->metadata[i].size[res];
+                    img_index->offset[res] = db_file->metadata[i].offset[res];
+                    img_index->size[res] = db_file->metadata[i].size[res];
                 }
                 return 0;
             }
         }
     }
     // No duplicates
-    img_index.offset[RES_ORIG] = 0;
+    img_index->offset[RES_ORIG] = 0;
     return 0;
 }
 
