651b61544e926e809488044d57f1bc4b0442cd33
panv <panv@hotmail.fr>
2016-05-02 15:17:05 +0200

    Add error checks in do_insert

--- done/pictDBM/db_insert.c
+++ done/pictDBM/db_insert.c
@@ -5,12 +5,19 @@
 int do_insert(const char* new_image, size_t size, const char* pict_id,
               struct pictdb_file* db_file)
 {
+    if (new_image == NULL || pict_id == NULL || db_file == NULL
+        || size == 0) {
+        return ERR_INVALID_ARGUMENT;
+    }
+    if (strlen(pict_id) == 0 || strlen(pict_id) > MAX_PIC_ID) {
+        return ERR_INVALID_PICID;
+    }
     if (!(db_file->header.num_files < db_file->header.max_files)) {
         return ERR_FULL_DATABASE;
     }
 
     uint32_t idx_new = 0; //find index of first empty metadata
-    while (db_file->metadata[idx_new].is_valid != EMPTY) {
+    while (db_file->metadata[idx_new].is_valid == NON_EMPTY) {
         ++idx_new;
     }
     struct pict_metadata* empty = &db_file->metadata[idx_new]; // convenience
@@ -24,21 +31,35 @@ int do_insert(const char* new_image, size_t size, const char* pict_id,
     if (dedup_err) {
         return dedup_err;
     } else if (empty->offset[RES_ORIG] == 0) {
-        fseek(db_file->fpdb, 0, SEEK_END); //CC
-        empty->offset[RES_ORIG] = ftell(db_file->fpdb); //CC
-        fwrite(new_image, size, 1, db_file->fpdb); //CC
+        int ret = fseek(db_file->fpdb, 0, SEEK_END) == 0 ? 0 : ERR_IO;
+        if (ret == 0) {
+            empty->offset[RES_ORIG] = ftell(db_file->fpdb);
+            ret = fwrite(new_image, size, 1, db_file->fpdb) == 1 ? 0 : ERR_IO;
+        }
+        if (ret != 0) {
+            return ret;
+        }
     }
     int found_res = get_resolution(&empty->res_orig[0], &empty->res_orig[1],
-                                   new_image, size); //CC
+                                   new_image, size);
+    if (found_res != 0) {
+        return found_res;
+    }
     ++db_file->header.db_version;
     ++db_file->header.num_files;
-    fseek(db_file->fpdb, 0, SEEK_SET);
-    fwrite(&db_file->header, sizeof(struct pictdb_header), 1, db_file->fpdb); //C
+    int ret = fseek(db_file->fpdb, 0, SEEK_SET) == 0 ? 0 : ERR_IO;
+    if (ret == 0) {
+        ret = fwrite(&db_file->header, sizeof(struct pictdb_header),
+                     1, db_file->fpdb) == 1 ? 0 : ERR_IO;
+    }
 
-    // PROBLEM: find where to write metadata. Is this correct?
-    uint64_t meta_offset = sizeof(struct pictdb_header) +
-                           idx_new * sizeof(struct pict_metadata);
-    fseek(db_file->fpdb, meta_offset, SEEK_SET); //CC
-    fwrite(empty, sizeof(struct pict_metadata), 1, db_file->fpdb); //C
-    return 0;
+    if (ret == 0) {
+        uint64_t meta_offset = sizeof(struct pictdb_header) +
+                               idx_new * sizeof(struct pict_metadata);
+        ret = fseek(db_file->fpdb, meta_offset, SEEK_SET) == 0 ? 0 : ERR_IO;
+        if (ret == 0) {
+            ret = fwrite(empty, sizeof(struct pict_metadata), 1, db_file->fpdb) == 1 ? 0 : ERR_IO;
+        }
+    }
+    return ret;
 }
