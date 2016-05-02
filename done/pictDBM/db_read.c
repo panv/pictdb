aa9a90590a120f83eec2a439da44fdbf00b18d23
panv <panv@hotmail.fr>
2016-05-02 16:50:31 +0200

    Add argument check to do_read

--- done/pictDBM/db_read.c
+++ done/pictDBM/db_read.c
@@ -1,9 +1,19 @@
 #include "pictDB.h"
 #include "image_content.h"
 
-int do_read(const char* pict_id, int resolution, char** image_address,
+int do_read(const char* pict_id, int resolution, char** image,
             uint32_t* size, struct pictdb_file* db_file)
 {
+    if (pict_id == NULL || db_file == NULL) {
+        return ERR_INVALID_ARGUMENT;
+    }
+    if (strlen(pict_id) == 0 || strlen(pict_id) > MAX_PIC_ID) {
+        return ERR_INVALID_PICID;
+    }
+    if (db_file->header.num_files == 0) {
+        return ERR_FILE_NOT_FOUND;
+    }
+
     uint32_t idx = 0;
     for (uint32_t i = 0; i < db_file->header.num_files; ++i) {
         if (db_file->metadata[i].is_valid == NON_EMPTY &&
@@ -22,18 +32,17 @@ int do_read(const char* pict_id, int resolution, char** image_address,
             return resized;
         }
     }
-    char* image_buffer = calloc(to_read->size[resolution], sizeof(char)); //CC
-    if (image_buffer == NULL) {
+    *image = calloc(to_read->size[resolution], sizeof(char)); //CC
+    if (*image == NULL) {
         return ERR_OUT_OF_MEMORY;
     }
     if (fseek(db_file->fpdb, to_read->offset[resolution], SEEK_SET) ||
-        fread(image_buffer, to_read->size[resolution], sizeof(char),
+        fread(*image, to_read->size[resolution], sizeof(char),
               db_file->fpdb) != to_read->size[resolution]) {
-        free(image_buffer);
+        free(*image);
         return ERR_IO;
     }
     *size = to_read->size[resolution];
-    *image_address = image_buffer;
     return 0;
 }
 
--- done/pictDBM/pictDB.h
+++ done/pictDBM/pictDB.h
@@ -216,12 +216,12 @@ int resolution_atoi(const char* resolution);
  *
  * @param pict_id       The ID of the image.
  * @param resolution    The resolution of the image.
- * @param image_address The memory address of the image.
+ * @param image         The destination of the image.
  * @param size          The size of the image, in bytes.
  * @param db_file       The database.
  * @return 0 if the read was successful, an error code otherwise.
  */
-int do_read(const char* pict_id, int resolution, char** image_address,
+int do_read(const char* pict_id, int resolution, char** image,
             uint32_t* size, struct pictdb_file* db_file);
 
 /**
