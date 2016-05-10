7a3df58a46129a9f01cee6b14c6db9c92db77884
panv <panv@hotmail.fr>
2016-05-10 18:43:12 +0200

    Modify dedup to check all database for duplicates

--- done/pictDBM/dedup.c
+++ done/pictDBM/dedup.c
@@ -23,6 +23,7 @@ int do_name_and_content_dedup(struct pictdb_file* db_file, uint32_t index)
     }
 
     struct pict_metadata* img_index = &db_file->metadata[index];
+    int found = 0;
     for (size_t i = 0; i < db_file->header.max_files; ++i) {
         // For all valid images other than the one at index
         if (i != index && db_file->metadata[i].is_valid == NON_EMPTY) {
@@ -30,18 +31,23 @@ int do_name_and_content_dedup(struct pictdb_file* db_file, uint32_t index)
             if (strncmp(db_file->metadata[i].pict_id,
                         img_index->pict_id, MAX_PIC_ID) == 0) {
                 return ERR_DUPLICATE_ID;
-            } else if (hashcmp(db_file->metadata[i].SHA, img_index->SHA) == 0) {
+            } else if (found == 0 &&
+                       hashcmp(db_file->metadata[i].SHA, img_index->SHA) == 0) {
                 // Two images with the same hash: deduplication
                 for (size_t res = 0; res < NB_RES; ++res) {
                     img_index->offset[res] = db_file->metadata[i].offset[res];
                     img_index->size[res] = db_file->metadata[i].size[res];
                 }
-                return 0;
+                found = 1;
             }
         }
     }
-    // No duplicates
-    img_index->offset[RES_ORIG] = 0;
+
+    // No duplicates found
+    if (found == 0) {
+        img_index->offset[RES_ORIG] = 0;
+    }
+
     return 0;
 }
 
