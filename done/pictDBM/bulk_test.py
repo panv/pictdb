eb727d4a2c62d3cc7d079c0d929417422b41b278
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-05-15 21:02:02 +0200

    Update test script

--- done/pictDBM/bulk_test.py
+++ done/pictDBM/bulk_test.py
@@ -2,7 +2,6 @@ import os
 from sys import argv
 from subprocess import getstatusoutput
 import random
-from datetime import datetime
 
 executable = argv[1]
 pics_path = argv[2]
@@ -10,7 +9,8 @@ db = "dbTEST" #+ datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 allpics = [pics_path + "/" + filename for filename in os.listdir(pics_path) if filename.endswith(".jpg")]
 
-assert getstatusoutput("./" + executable + " create " + db + " -max_files 1000")[0] == 0
+assert getstatusoutput("./" + executable + " create " + db + " -max_files 10000")[0] == 0
+print("Database created")
 
 def insert_cmd(pict_id, filename):
     return getstatusoutput("./" + executable + " insert " + db + " " + pict_id + " " + filename)[0]
@@ -19,35 +19,34 @@ def read_cmd(pict_id, resolution):
     return getstatusoutput("./" + executable + " read " + db + " " + pict_id + " " + resolution)[0]
 
 def addAll(pics):
-    success = 0
     for index, image in enumerate(pics):
-        success += insert_cmd(str(index), image)
-    assert success == 0
+        exit_code = insert_cmd(str(index), image)
+        assert exit_code == 0, "Could not add "+ image + " with id " + str(index) + " error code: " + str(exit_code)
     print("Added all pictures in folder correctly")
 
 def resizeAll(allpics):
-    success = 0
     for index, image in enumerate(allpics):
         for resolution in ["thumb", "small"]:
-            success += read_cmd(str(index), resolution)
-    assert success == 0
+            exit_code = read_cmd(str(index), resolution)
+    assert exit_code == 0, "Could not read " + str(index) + " from db, exit code: " + output
     getstatusoutput("rm *small.jpg")
     getstatusoutput("rm *thumb.jpg")
     print("Resized all pictures to all resolutions correctly")
 
 def addAlreadyPresentId():
     for i in range(0, random.randint(0, len(allpics))):
-        assert insert_cmd(str(random.randint(0, len(allpics) - 1)), random.choice(allpics)) != 0
+        assert insert_cmd(str(random.randint(0, len(allpics) - 1)), random.choice(allpics)) != 0, "Error: dded image with id already in db!"
     print("Inserting a random number of already present id returns error")
 
 def addAlreadyPresentimage(pics):
     l = len(pics)
-    length_before_add = os.stat(db).st_size
-    random_pic = random.choice(pics)
-    output = insert_cmd(str(random.randint(l + 1, l + 100)), random_pic)
-    assert output == 0
+    for i in range(1, random.randint(1, 20)):
+        length_before_add = os.stat(db).st_size
+        random_pic = random.choice(pics)
+        output = insert_cmd(str(random.randint(l + 1, l + 100)), random_pic)
+        assert output == 0, "Error: adding already present image returns error"
+        assert os.stat(db).st_size < length_before_add + os.stat(random_pic).st_size
     print("Adding already present image does not return error")
-    assert os.stat(db).st_size < length_before_add + os.stat(random_pic).st_size
     print("Adding already present image deduplicates")
 
 addAll(allpics)
